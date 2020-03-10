4bba5d88214170e26abd46d5077131572b14dadd
metinyldz <>
2020-03-10 17:07:49 +0300

    Created View and ViewController Class

---
+++ Vmp/View.swift
@@ -0,0 +1,9 @@
+//
+//  View.swift
+//  Vmp
+//
+//  Created by Metin Yıldız on 10.03.2020.
+//  Copyright © 2020 Metin Yıldız. All rights reserved.
+//
+
+import Foundation
---
+++ Vmp/ViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ViewController.swift
+//  Vmp
+//
+//  Created by Metin Yıldız on 10.03.2020.
+//  Copyright © 2020 Metin Yıldız. All rights reserved.
+//
+
+import Foundation
